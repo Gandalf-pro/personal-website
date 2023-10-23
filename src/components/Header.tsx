ea863a10e6f0bbd127764763f1b72f89a229f61e
Gandalf-pro <asoc04@hotmail.com>
2023-10-24 01:05:20 +0300

    header underline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,66 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import HeaderUnderline from "./HeaderUnderline";
 
 const Header = () => {
   return (
-    <div className="fixed z-50 mx-auto flex h-20 w-full justify-around bg-gradient-to-b from-[#1f2733] to-[#1f2838] px-4 py-4 text-xl shadow shadow-pink-500">
+    <div className="fixed z-50 mx-auto flex h-20 w-full items-center justify-around bg-gradient-to-b from-[#1f2733] to-[#1f2838] px-4 py-4 text-xl shadow shadow-pink-500">
       <Link
         href="/"
-        className="inline-flex items-center justify-center font-semibold underline drop-shadow"
+        className="group inline-flex h-fit items-center justify-center drop-shadow"
       >
         <Image alt="Logo" src="/logo-transparent.png" width={48} height={48} />
       </Link>
-      <Link
-        href="/about"
-        className="inline-flex items-center justify-center font-semibold underline drop-shadow"
-      >
-        Projects
-      </Link>
-      <Link
-        href="/#about"
-        onClick={(e) => {
-          const aboutSection = document.querySelector("#about");
-          if (aboutSection) {
-            e.preventDefault();
-            aboutSection.scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-          }
-        }}
-        className="inline-flex items-center justify-center font-semibold underline drop-shadow"
-      >
-        About
-      </Link>
-      <Link
-        href="/#contact"
-        onClick={(e) => {
-          const aboutSection = document.querySelector("#contact");
-          if (aboutSection) {
-            e.preventDefault();
-            aboutSection.scrollIntoView({
-              behavior: "smooth",
-              block: "center",
-            });
-          }
-        }}
-        className="inline-flex items-center justify-center font-semibold underline drop-shadow"
-      >
-        Contact
-      </Link>
-      <Link
-        href="/blog"
-        className="inline-flex items-center justify-center font-semibold underline drop-shadow"
-      >
-        Blog
-      </Link>
+      <div className="flex gap-3">
+        <Link
+          href="/#about"
+          onClick={(e) => {
+            const aboutSection = document.querySelector("#about");
+            if (aboutSection) {
+              e.preventDefault();
+              aboutSection.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+              });
+            }
+          }}
+          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+        >
+          About
+          <HeaderUnderline />
+        </Link>
+        <Link
+          href="/#contact"
+          onClick={(e) => {
+            const aboutSection = document.querySelector("#contact");
+            if (aboutSection) {
+              e.preventDefault();
+              aboutSection.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+              });
+            }
+          }}
+          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+        >
+          Contact
+          <HeaderUnderline />
+        </Link>
+        <Link
+          href="/projects"
+          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+        >
+          Projects
+          <HeaderUnderline />
+        </Link>
+        <Link
+          href="/blog"
+          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+        >
+          Blog
+          <HeaderUnderline />
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ src/components/HeaderUnderline.tsx
@@ -0,0 +1,7 @@
+const HeaderUnderline = () => {
+  return (
+    <div className="absolute -bottom-[3px] h-[3px] w-1 rounded-full bg-gradient-to-r from-red-600 to-pink-600 shadow shadow-pink-600/50 transition-all delay-75 duration-200 ease-in-out group-hover:w-full" />
+  );
+};
+
+export default HeaderUnderline;
