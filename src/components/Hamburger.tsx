40793d5cf648177406e34f7316ba7eeb1109f5b9
Gandalf-pro <asoc04@hotmail.com>
2023-10-24 01:27:58 +0300

    hamburger working

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import type { Dispatch, SetStateAction } from "react";
 
 export interface HamburgerProps {
   isVisible: boolean;
-  setIsVisible: (val: boolean) => void;
+  setIsVisible: Dispatch<SetStateAction<boolean>>;
 }
 
 const Hamburger = ({ isVisible, setIsVisible }: HamburgerProps) => {
-  return (
-    <button className="group relative">
-      <div className="relative flex h-[50px] w-[50px] transform items-center justify-center overflow-hidden rounded-full bg-slate-700 shadow-md ring-0 ring-gray-300 ring-opacity-30 transition-all duration-200 hover:ring-8 group-focus:ring-4">
-        <div className="flex h-[20px] w-[20px] origin-center transform flex-col justify-between overflow-hidden transition-all duration-300">
-          <div className="h-[2px] w-7 origin-left transform bg-white transition-all duration-300 group-focus:translate-x-10"></div>
-          <div className="h-[2px] w-7 transform rounded bg-white transition-all delay-75 duration-300 group-focus:translate-x-10"></div>
-          <div className="h-[2px] w-7 origin-left transform bg-white transition-all delay-150 duration-300 group-focus:translate-x-10"></div>
+  const handleClick = () => {
+    setIsVisible((val) => !val);
+  };
 
-          <div className="absolute top-2.5 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-            <div className="absolute h-[2px] w-5 rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:rotate-45"></div>
-            <div className="absolute h-[2px] w-5 -rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:-rotate-45"></div>
-          </div>
-        </div>
-      </div>
+  return (
+    <button
+      onClick={handleClick}
+      className="flex flex-col items-center justify-center text-white"
+    >
+      <span
+        className={`block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+          isVisible ? "translate-y-1 rotate-45" : "-translate-y-0.5"
+        }`}
+      ></span>
+      <span
+        className={`my-0.5 block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+          isVisible ? "opacity-0" : "opacity-100"
+        }`}
+      ></span>
+      <span
+        className={`block h-0.5 w-6 rounded-sm bg-white transition-all duration-300 ease-out ${
+          isVisible ? "-translate-y-1 -rotate-45" : "translate-y-0.5"
+        }`}
+      ></span>
     </button>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,72 +3,92 @@ import Link from "next/link";
 import HeaderUnderline from "./HeaderUnderline";
 import { useState } from "react";
 import Hamburger from "./Hamburger";
+import { cn } from "~/utils/cn";
 
 const Header = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  return (
-    <div className="fixed z-50 mx-auto flex h-20 w-full items-center justify-around bg-gradient-to-b from-[#1f2733] to-[#1f2838] px-4 py-4 text-xl shadow shadow-pink-500">
+  const innerLinks = (
+    <>
       <Link
-        href="/"
-        className="group inline-flex h-fit items-center justify-center drop-shadow"
+        href="/#about"
+        onClick={(e) => {
+          const aboutSection = document.querySelector("#about");
+          if (aboutSection) {
+            e.preventDefault();
+            aboutSection.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+            });
+          }
+        }}
+        className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
       >
-        <Image alt="Logo" src="/logo-transparent.png" width={48} height={48} />
+        About
+        <HeaderUnderline />
       </Link>
-      {/* Sm hamburger */}
-      <div className="block sm:hidden">
-        <Hamburger isVisible={isVisible} setIsVisible={setIsVisible} />
+      <Link
+        href="/#contact"
+        onClick={(e) => {
+          const aboutSection = document.querySelector("#contact");
+          if (aboutSection) {
+            e.preventDefault();
+            aboutSection.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+            });
+          }
+        }}
+        className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+      >
+        Contact
+        <HeaderUnderline />
+      </Link>
+      <Link
+        href="/projects"
+        className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+      >
+        Projects
+        <HeaderUnderline />
+      </Link>
+      <Link
+        href="/blog"
+        className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
+      >
+        Blog
+        <HeaderUnderline />
+      </Link>
+    </>
+  );
+
+  return (
+    <div
+      className={`fixed z-50 mx-auto w-full bg-gradient-to-b from-[#1f2733] to-[#1f2838] px-4 py-4 text-xl shadow shadow-pink-500 transition-all sm:h-20 ${
+        isVisible ? "h-fit" : "h-20"
+      }`}
+    >
+      <div className="flex h-full w-full items-center justify-around">
+        <Link
+          href="/"
+          className="group inline-flex h-fit items-center justify-center drop-shadow"
+        >
+          <Image
+            alt="Logo"
+            src="/logo-transparent.png"
+            width={48}
+            height={48}
+          />
+        </Link>
+        {/* Sm hamburger */}
+        <div className="block sm:hidden">
+          <Hamburger isVisible={isVisible} setIsVisible={setIsVisible} />
+        </div>
+
+        <div className="hidden gap-3 sm:flex">{innerLinks}</div>
       </div>
-      <div className="hidden gap-3 sm:flex">
-        <Link
-          href="/#about"
-          onClick={(e) => {
-            const aboutSection = document.querySelector("#about");
-            if (aboutSection) {
-              e.preventDefault();
-              aboutSection.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-              });
-            }
-          }}
-          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
-        >
-          About
-          <HeaderUnderline />
-        </Link>
-        <Link
-          href="/#contact"
-          onClick={(e) => {
-            const aboutSection = document.querySelector("#contact");
-            if (aboutSection) {
-              e.preventDefault();
-              aboutSection.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-              });
-            }
-          }}
-          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
-        >
-          Contact
-          <HeaderUnderline />
-        </Link>
-        <Link
-          href="/projects"
-          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
-        >
-          Projects
-          <HeaderUnderline />
-        </Link>
-        <Link
-          href="/blog"
-          className="group inline-flex h-fit items-center justify-center font-medium drop-shadow"
-        >
-          Blog
-          <HeaderUnderline />
-        </Link>
-      </div>
+      {isVisible && (
+        <div className="mt-3 flex flex-col gap-3 sm:hidden">{innerLinks}</div>
+      )}
     </div>
   );
 };
