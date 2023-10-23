39a35426a9d2282c4b0e07d8423eb27c71345d89
Gandalf-pro <asoc04@hotmail.com>
2023-10-24 02:25:19 +0300

    pages

---
+++ src/pages/blog.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import AppWrapper from "~/components/AppWrapper";
+
+const Blog: NextPage = () => {
+  return <AppWrapper>Blog</AppWrapper>;
+};
+
+export default Blog;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import AppWrapper from "~/components/AppWrapper";
+
+const Projects: NextPage = () => {
+  return <AppWrapper>Projects</AppWrapper>;
+};
+
+export default Projects;
