5caf5c81c8b1b1ac44942130f0572082535e456b
Gandalf-pro <asoc04@hotmail.com>
2023-10-24 00:30:59 +0300

    pointer for badges

--- src/components/TechnologiesShowcase.tsx
+++ src/components/TechnologiesShowcase.tsx
@@ -6,7 +6,7 @@ export interface TechnologiesShowcaseProps {
 
 const TechnologiesShowcase = (props: TechnologiesShowcaseProps) => {
   return (
-    <div className="rounded-3xl bg-slate-700 bg-opacity-60  px-4 py-2 text-lg drop-shadow hover:text-pink-600 hover:bg-opacity-40 transition-all font-medium">
+    <div className="cursor-pointer rounded-3xl bg-slate-700  bg-opacity-60 px-4 py-2 text-lg font-medium drop-shadow transition-all hover:bg-opacity-40 hover:text-pink-600">
       {props.text}
     </div>
   );
