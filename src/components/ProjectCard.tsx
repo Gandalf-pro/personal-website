7d406cdcd5c3df8171c6ec9847fc325bb9b66298
Gandalf-pro <asoc04@hotmail.com>
2023-10-24 23:17:22 +0300

    card improvements

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,5 @@
+import { IconCaretRight } from "@tabler/icons-react";
 import Link from "next/link";
-import React from "react";
 import { type ProjectsDataType } from "~/data/projectsData";
 
 const ProjectCard = ({
@@ -9,17 +9,25 @@ const ProjectCard = ({
   technologies,
 }: ProjectsDataType) => {
   return (
-    <div className="w-full rounded-xl bg-secondary p-3">
-      <h3 className="mb-4 text-xl font-bold">{name}</h3>
+    <div className="w-full rounded-xl bg-secondary p-3 shadow">
+      <h3 className="mb-4 text-2xl font-extrabold">{name}</h3>
       <div className="flex">
         {technologies.map((val) => (
-          <div key={val} className="rounded-full bg-slate-700/70 px-2 py-1">
+          <div
+            key={val}
+            className="rounded-full bg-slate-700/70 px-2 py-1 text-sm"
+          >
             {val}
           </div>
         ))}
       </div>
-      <div className="mb-3 mt-2">{description}</div>
-      <Link href={`/projects/${slug}`} className="px-2 py-1 bg-">Learn More</Link>
+      <div className="mb-5 mt-2 text-justify">{description}</div>
+      <Link
+        href={`/projects/${slug}`}
+        className="w-full inline-flex items-center justify-center gap-2 rounded-lg border-2 border-pink-600 px-4 py-1 text-lg shadow-sm shadow-pink-600 transition-colors hover:bg-gradient-to-br hover:from-pink-600 hover:to-red-600"
+      >
+        Learn More <IconCaretRight className="h-7 w-7" />
+      </Link>
     </div>
   );
 };
