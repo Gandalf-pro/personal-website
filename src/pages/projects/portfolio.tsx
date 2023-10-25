9d7a7a95889a45a894fd089a1fa30db58a09c380
Gandalf-pro <asoc04@hotmail.com>
2023-10-25 20:03:40 +0300

    portfolio page

---
+++ src/pages/projects/portfolio.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import ProjectDetailPageWrapper from "~/components/ProjectDetailPageWrapper";
+
+const Portfolio: NextPage = () => {
+  return (
+    <ProjectDetailPageWrapper slug="portfolio">
+      Not much to say really you are here now 😊
+    </ProjectDetailPageWrapper>
+  );
+};
+
+export default Portfolio;
