a58f0a08026bf3d70e3c4da12511f65df6b460ea
Gandalf-pro <asoc04@hotmail.com>
2023-10-25 19:57:12 +0300

    contact working

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,37 +1,123 @@
-import { IconMailFast } from "@tabler/icons-react";
+import { IconLoader2, IconMail, IconMailFast } from "@tabler/icons-react";
+import Link from "next/link";
+import { useState, type FormEvent } from "react";
+import { constantVariables } from "~/utils/constantVariables";
 import HomeSection from "./HomeSection";
 
 const ContactSection = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleSend = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setError("");
+      setSuccessMessage("");
+      setIsLoading(true);
+      const data = new FormData(e.currentTarget);
+      const body: Record<string, string> = {};
+      data.forEach((value, key) => (body[key] = value.toString()));
+
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      try {
+        const tmp = (await res.json()) as { message: string };
+        if (res.ok) {
+          setSuccessMessage(tmp.message);
+        } else {
+          setError(tmp.message);
+        }
+      } catch (error) {
+        setError("Got an error.");
+      }
+    } catch (error) {
+      console.error(error);
+      setError("Got an error.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <HomeSection title="📬 Contact" id="contact">
-      <div className="flex w-full flex-col gap-3">
-        <input
-          name="name"
-          id="name"
-          className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none"
-          placeholder="Name *"
-          maxLength={50}
-        />
-        <input
-          name="email"
-          type="email"
-          id="email"
-          className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none"
-          placeholder="Email *"
-          maxLength={60}
-        />
-        <textarea
-          className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none"
-          placeholder="Message *"
-          rows={5}
-          minLength={3}
-          maxLength={5000}
-        />
-        <button className="inline-flex w-full items-center justify-center gap-3 rounded-lg bg-black bg-opacity-50 p-2 px-4 py-3 text-lg font-semibold text-white hover:bg-opacity-70 focus:outline-none">
-          <IconMailFast className="h-8 w-8" />
-          Send
-        </button>
-      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          void handleSend(e);
+        }}
+      >
+        <div className="flex w-full flex-col gap-3">
+          {error && (
+            <div className="flex w-full gap-3">
+              <div className="flex-1 rounded-lg bg-destructive bg-opacity-50 p-2 px-4 py-4 text-white">
+                {error}
+              </div>
+              <Link
+                href={`mailto:${constantVariables.emailAddress}`}
+                className=" flex flex-col items-center justify-center rounded-lg border-2 border-pink-600 px-2 py-1"
+              >
+                Use Mail Instead?
+                <IconMail className="h-8 w-8" />
+              </Link>
+            </div>
+          )}
+          {successMessage && (
+            <div className="w-full rounded-lg bg-green-600 bg-opacity-50 p-2 px-4 py-4 text-white">
+              {successMessage}
+            </div>
+          )}
+          <input
+            name="name"
+            id="name"
+            className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            placeholder="Name *"
+            maxLength={50}
+            required
+            disabled={isLoading}
+          />
+          <input
+            name="email"
+            type="email"
+            id="email"
+            className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            placeholder="Email *"
+            maxLength={60}
+            required
+            disabled={isLoading}
+          />
+          <textarea
+            className="w-full rounded-lg bg-black bg-opacity-50 p-2 px-4 py-4 text-white focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            placeholder="Message *"
+            name="message"
+            rows={5}
+            minLength={3}
+            maxLength={3000}
+            required
+            disabled={isLoading}
+          />
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="inline-flex w-full items-center justify-center gap-3 rounded-lg bg-black bg-opacity-50 p-2 px-4 py-3 text-lg font-semibold text-white hover:bg-opacity-70 focus:outline-none disabled:pointer-events-none"
+          >
+            {isLoading ? (
+              <IconLoader2 className="h-6 w-6 animate-spin" />
+            ) : (
+              <>
+                <IconMailFast className="h-8 w-8" />
+                Send
+              </>
+            )}
+          </button>
+        </div>
+      </form>
     </HomeSection>
   );
 };
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,92 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { env } from "~/env.mjs";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+type ResponseData = {
+  message: string;
+};
+
+async function sendMailToDiscord(msg: string) {
+  const res = await fetch(env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      content: msg,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error();
+  }
+}
+
+function getSplitMessage(msg: string) {
+  let middle = Math.floor(msg.length / 2);
+  const before = msg.lastIndexOf(" ", middle);
+  const after = msg.indexOf(" ", middle + 1);
+
+  if (middle - before < after - middle) {
+    middle = before;
+  } else {
+    middle = after;
+  }
+
+  return [msg.substring(0, middle), msg.substring(middle + 1)] as const;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method !== "POST") {
+    return res.status(405);
+  }
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { name, email, message } = req.body;
+
+    if (
+      !name ||
+      typeof name !== "string" ||
+      name.length < 3 ||
+      name.length > 50
+    ) {
+      return res.status(400).json({ message: "Name is needed" });
+    }
+    if (
+      !email ||
+      typeof email !== "string" ||
+      email.length < 3 ||
+      email.length > 60 ||
+      !emailRegex.test(email)
+    ) {
+      return res.status(400).json({ message: "Email is needed" });
+    }
+    if (
+      !message ||
+      typeof message !== "string" ||
+      message.length < 3 ||
+      message.length > 3000
+    ) {
+      return res.status(400).json({ message: "Message is needed" });
+    }
+
+    if (message.length > 1600) {
+      const split = getSplitMessage(message);
+      await sendMailToDiscord(`${name} - ${email}\n${split[0]}`);
+      await sendMailToDiscord(split[1]);
+    } else {
+      await sendMailToDiscord(`${name} - ${email}\n${message}`);
+    }
+
+    res
+      .status(200)
+      .json({ message: "Received your message! Thank you for reaching out." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "There was an error." });
+  }
+}
