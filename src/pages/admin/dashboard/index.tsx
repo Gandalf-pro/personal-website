d7f0e1bbbaa6092fd50c516592799f42d83be170
Gandalf-pro <asoc04@hotmail.com>
2023-11-01 02:26:15 +0300

    search

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import DashboardBlogsListCard from "~/components/DashboardBlogsListCard";
 import DashboardWrapper from "~/components/DashboardWrapper";
 import { api } from "~/utils/api";
 
 const AdminDashboard = () => {
   const blogs = api.blogs.getListOfBlogs.useQuery();
+  const [searchText, setSearchText] = useState("");
+
+  const filteredBlogs = useMemo(() => {
+    const searchTextLowerCased = searchText.toLowerCase();
+    return (
+      blogs.data?.blogs.filter((val) => {
+        return (
+          val.title.toLowerCase().includes(searchTextLowerCased) ||
+          val.author.name.toLowerCase().includes(searchTextLowerCased)
+        );
+      }) ?? []
+    );
+  }, [searchText]);
 
   return (
-    <DashboardWrapper className="container mx-auto" title="Dashboard">
-      <div className="mt-12 grid sm:grid-cols-2 grid-cols-1 gap-3">
-        {blogs.data?.blogs.map((blog) => (
+    <DashboardWrapper
+      className="container mx-auto px-2 sm:px-0"
+      title="Dashboard"
+    >
+      <input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.currentTarget.value)}
+        placeholder="Search by title or author..."
+        className="mt-12 w-full rounded bg-black/60 px-6 py-4 focus:outline-none"
+      />
+      <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2">
+        {filteredBlogs.map((blog) => (
           <DashboardBlogsListCard key={blog.id} blog={blog} />
         ))}
       </div>
