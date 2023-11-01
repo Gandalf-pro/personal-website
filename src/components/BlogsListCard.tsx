d26b3db4093c90952bafc25dd69f6d04307524a4
Gandalf-pro <asoc04@hotmail.com>
2023-11-02 02:40:33 +0300

    display skills in card as well

--- src/components/BlogsListCard.tsx
+++ src/components/BlogsListCard.tsx
@@ -20,6 +20,18 @@ const BlogsListCard = ({ blog }: BlogsListCardProps) => {
     <Link href={`/blog/${blog.slug}`}>
       <div className="rounded bg-secondary p-3 shadow hover:bg-secondary hover:shadow-xl">
         <div className="mb-3 text-2xl font-semibold">{blog.title}</div>
+        {blog.skills.length > 0 && (
+          <div className="-mt-1 mb-3 flex gap-1 overflow-x-auto">
+            {blog.skills.map((val) => (
+              <div
+                key={val.skillId}
+                className="whitespace-nowrap rounded-full bg-slate-700/70 px-2 py-1 text-sm text-primary/80"
+              >
+                {val.skill.name}
+              </div>
+            ))}
+          </div>
+        )}
         <div className="flex w-full flex-wrap items-center justify-between text-lg">
           <span className="inline-flex gap-1">
             <IconUser />
