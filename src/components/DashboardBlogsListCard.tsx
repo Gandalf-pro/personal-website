8db232147d2e65ddf9a3fc39a0c3db720299a722
Gandalf-pro <asoc04@hotmail.com>
2023-11-01 22:58:42 +0300

    blog active flag on list

--- src/components/DashboardBlogsListCard.tsx
+++ src/components/DashboardBlogsListCard.tsx
@@ -1,9 +1,11 @@
-import { type RouterOutputs } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { IconCalendarPlus, IconRotate2, IconUser } from "@tabler/icons-react";
 import Link from "next/link";
 import { memo } from "react";
+import { Switch } from "./Switch";
+import { cn } from "~/utils/cn";
 dayjs.extend(relativeTime);
 
 export interface DashboardBlogsListCardProps {
@@ -11,12 +13,35 @@ export interface DashboardBlogsListCardProps {
 }
 
 const DashboardBlogsListCard = ({ blog }: DashboardBlogsListCardProps) => {
+  const apiContext = api.useUtils();
+  const upsertBlog = api.blogs.upsertBlog.useMutation({
+    onSuccess() {
+      void apiContext.blogs.invalidate();
+    },
+  });
+
   return (
     <Link
-      className="rounded bg-black/50 p-4 shadow hover:bg-black/70 hover:shadow-xl"
+      className={cn([
+        "rounded bg-black/50 p-4 shadow hover:bg-black/70 hover:shadow-xl",
+        upsertBlog.isLoading && "pointer-events-none bg-black/60 blur-[1px]",
+      ])}
       href={`/admin/dashboard/blog/${blog.id}`}
     >
-      <div className="mb-3 text-2xl font-semibold">{blog.title}</div>
+      <div className="mb-3 inline-flex w-full items-center justify-between">
+        <div className="text-2xl font-semibold">{blog.title}</div>
+        <Switch
+          checked={blog.active}
+          disabled={upsertBlog.isLoading}
+          onClick={(e) => {
+            e.preventDefault();
+            upsertBlog.mutate({
+              id: blog.id,
+              active: !blog.active,
+            });
+          }}
+        />
+      </div>
       <div className="flex w-full flex-wrap items-center justify-between text-lg">
         <span className="inline-flex gap-1">
           <IconUser />
