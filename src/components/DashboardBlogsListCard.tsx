362ed5b0ab8e1688b6e3147c3e900d464778a62e
Gandalf-pro <asoc04@hotmail.com>
2023-11-02 03:45:21 +0300

    delete blog

--- src/components/DashboardBlogsListCard.tsx
+++ src/components/DashboardBlogsListCard.tsx
@@ -1,11 +1,17 @@
 import { api, type RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { IconCalendarPlus, IconRotate2, IconUser } from "@tabler/icons-react";
+import {
+  IconCalendarPlus,
+  IconRotate2,
+  IconTrash,
+  IconUser,
+} from "@tabler/icons-react";
 import Link from "next/link";
 import { memo } from "react";
 import { Switch } from "./ui/Switch";
 import { cn } from "~/utils/cn";
+import { Button } from "./ui/Button";
 dayjs.extend(relativeTime);
 
 export interface DashboardBlogsListCardProps {
@@ -19,20 +25,28 @@ const DashboardBlogsListCard = ({ blog }: DashboardBlogsListCardProps) => {
       void apiContext.blogs.invalidate();
     },
   });
+  const deleteBlog = api.blogs.deleteBlog.useMutation({
+    onSuccess() {
+      void apiContext.blogs.invalidate();
+    },
+  });
+
+  const isLoading = upsertBlog.isLoading || deleteBlog.isLoading;
 
   return (
     <Link
       className={cn([
         "rounded bg-black/50 p-4 shadow hover:bg-black/70 hover:shadow-xl",
-        upsertBlog.isLoading && "pointer-events-none bg-black/60 blur-[1px]",
+        isLoading && "pointer-events-none bg-black/60 blur-[1px]",
       ])}
       href={`/admin/dashboard/blog/${blog.id}`}
     >
       <div className="mb-3 inline-flex w-full items-center justify-between">
         <div className="text-2xl font-semibold">{blog.title}</div>
         <Switch
+          className="mr-2"
           checked={blog.active}
-          disabled={upsertBlog.isLoading}
+          disabled={isLoading}
           onClick={(e) => {
             e.preventDefault();
             upsertBlog.mutate({
@@ -41,6 +55,20 @@ const DashboardBlogsListCard = ({ blog }: DashboardBlogsListCardProps) => {
             });
           }}
         />
+        <Button
+          variant="destructive"
+          size="icon"
+          className=""
+          disabled={isLoading}
+          onClick={(e) => {
+            e.preventDefault();
+            deleteBlog.mutate({
+              id: blog.id,
+            });
+          }}
+        >
+          <IconTrash className="" />
+        </Button>
       </div>
       <div className="flex w-full flex-wrap items-center justify-between text-lg">
         <span className="inline-flex gap-1">
